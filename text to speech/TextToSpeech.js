5e4a5d3ae65460b5154516d1eb8310bb3bd06484
ktonh123 <60385614+ktonh123@users.noreply.github.com>
2020-09-12 16:36:44 +0700

    Update TextToSpeech.js

--- text to speech/TextToSpeech.js
+++ text to speech/TextToSpeech.js
@@ -1,6 +1,11 @@
-var text = "Xin chào";
-text = encodeURIComponent(text);
-console.log(text);
-var url = "https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=vi&q=" + text;
+let text = ["Tôi ", "tên ", "là ", "Hiếu"];
+let audio = "";
+for (let i = 0; i < text.length; i++) {
+    audio += text[i];
+}
+console.log(audio);
+audio = encodeURIComponent(audio);
+console.log(audio);
+var url = "https://translate.google.com.vn/translate_tts?ie=UTF-8&q=" + audio + "&tl=vi&client=tw-ob";
 console.log(url);
 $('audio').attr('src', url).get(0).play();
