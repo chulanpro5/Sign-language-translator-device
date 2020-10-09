475aa1369e986364d3eab4b5cd891240ef532af0
chulanpro5 <68040131+chulanpro5@users.noreply.github.com>
2020-10-09 22:25:34 +0700

    add speech to video (test 01)

--- autoPlayVideo/HelloWorld.js
+++
@@ -1,2 +0,0 @@
-var audio = new Audio('test.mp3');
-audio.play();
---
+++ speech to video/sketch.js
@@ -0,0 +1,85 @@
+let lang = navigator.language || 'vi-VN';
+let speechRec = new p5.SpeechRec(lang, gotSpeech);
+
+let continuous = true;
+let interim = false;
+
+speechRec.start(continuous, interim);
+
+function gotSpeech() {
+  if (speechRec.resultValue) {
+    //createP(speechRec.resultString);
+    let str = speechRec.resultString;
+    console.log(str);
+    playVideo(str);
+  }
+}
+function playVideo(str)
+{
+  let inputText=[];
+  let text='';
+  for (let i=0;i<str.length; i++) {
+    if (str[i] == ' ') {
+      kq = text.toLowerCase();
+      inputText.push(kq);
+      text = '';
+    }
+    else text = text + str[i];
+  }
+  kq = text.toLowerCase();
+  inputText.push(kq);
+
+  //for (let i=0; i<inputText.length; i++)
+  //  console.log(inputText[i]);
+
+  autoPlay(inputText);
+}
+
+
+function autoPlay(inputText)
+{
+  for (let i=0; i<inputText.length; i++)
+    console.log(inputText[i]);
+
+  //let nameWord = ["xin" , "chào" , "bạn"];
+  let playlist = [];
+  let type = ".mp4";
+  for (let i = 0; i < inputText.length; i++) {
+      let nameVideo = "";
+      let normalize = encodeURIComponent(inputText[i]);
+      for(let j = 0 ; j < normalize.length ; j++)
+          if(normalize[j] != '%')
+              nameVideo = nameVideo + normalize[j];
+      playlist.push( nameVideo + type);
+  }
+  console.log(playlist);
+  let video = Video(playlist[0], true);
+  let i = 0;
+  let o = 0;
+  video.addEventListener('ended', function() {
+      i++;
+      console.log(i);
+      if (i > playlist.length) o = 1;
+      if (o === 0) {
+          console.log(i);
+          video.src = playlist[i];
+          video.play();
+      }
+  }, true);
+  video.height = 280;
+  video.width = 500;
+  video.muted = true;
+  video.src = playlist[0];
+  console.log(video.src);
+  video.play();
+}
+function Video(src, append) {
+    let v = document.createElement("video");
+    if (src != "") {
+        v.src = src;
+    }
+    if (append == true) {
+        document.body.appendChild(v);
+    }
+    return v;
+}
