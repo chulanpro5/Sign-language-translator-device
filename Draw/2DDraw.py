98d12023c0513d496fcd7a25b7ce00cf07db9021
ktonh123 <60385614+ktonh123@users.noreply.github.com>
2021-03-15 11:07:17 +0700

    nothing

--- Draw/2DDraw.py
+++ Draw/2DDraw.py
@@ -4,20 +4,43 @@ import numpy as np
 import time
 import math
 f = open('Data2D.json',)
-data = json.load(f)
-x_number_list = []
-y_number_list = []
+Data = json.load(f)
 
-plt.title("Extract Number Root ")
-plt.xlabel("Number")
-plt.ylabel("Extract Root of Number")
 
-print(len(data))
+clor = ['blue' , 'green' , 'red']
+plt.title("Chuyển động sau khi xử lí")
+plt.xlabel("x axis")
+plt.ylabel("z axis")
 
-for curData in data:
-    x_number_list.append(curData['hands'][0]['palmPosition'][0])
-    y_number_list.append(abs(curData['hands'][0]['palmPosition'][2]))
+for id in range(3 , 6):
+    data = Data[id]
+    x_number_list = []
+    y_number_list = []
+    
+    tempX = data[4]['hands'][0]['palmPosition'][0]
+    tempY = data[4]['hands'][0]['palmPosition'][2]
+    for curData in data:
+        
+        x_number_list.append(curData['hands'][0]['palmPosition'][0] - tempX - 230)
+        y_number_list.append(-curData['hands'][0]['palmPosition'][2] + tempY - 46)
 
-plt.scatter(x_number_list, y_number_list, s=100)
+    print(x_number_list)
+    print(y_number_list)
+    id = id % 3
+    plt.scatter(x_number_list, y_number_list, s=100 , color = clor[id])
+
+    coor = [-230 , -138 , -46 , 46 , 138 , 230]
+
+    xx = [-230 , 230]
+    yyy = [-230 , 230]
+    for i in coor:
+        yy = []
+        xxx = []
+        yy.append(i)
+        yy.append(i)
+        xxx = yy
+        plt.plot(xx , yy , color = 'black')
+        plt.plot(xxx , yyy , color= 'black')
+
 
 plt.show()
--- Draw/3DDraw.py
+++ Draw/3DDraw.py
@@ -16,7 +16,7 @@ ax.set_xlabel("x axis")
 ax.set_ylabel("y axis")
 ax.set_zlabel("z axis")
 
-Finger = ["pipPosition", "dipPosition", "mcpPosition"]
+Finger = ["tipPosition" , "pipPosition", "dipPosition", "mcpPosition"]
 
 ax.scatter(xs = data[0]["hands"][0]["palmPosition"][0] , ys = data[0]["hands"][0]["palmPosition"][1] , zs = data[0]["hands"][0]["palmPosition"][2], s = 100)
 
@@ -25,14 +25,17 @@ for i in range(0 , 5 , 1):
     y = []
     z = []
 
+    x.append(data[0]["pointables"][i]["tipPosition"][0])
     x.append(data[0]["pointables"][i]["dipPosition"][0])
     x.append(data[0]["pointables"][i]["pipPosition"][0])
     x.append(data[0]["pointables"][i]["mcpPosition"][0])
 
+    y.append(data[0]["pointables"][i]["tipPosition"][1])
     y.append(data[0]["pointables"][i]["dipPosition"][1])
     y.append(data[0]["pointables"][i]["pipPosition"][1])
     y.append(data[0]["pointables"][i]["mcpPosition"][1])   
 
+    z.append(data[0]["pointables"][i]["tipPosition"][2])
     z.append(data[0]["pointables"][i]["dipPosition"][2])
     z.append(data[0]["pointables"][i]["pipPosition"][2])
     z.append(data[0]["pointables"][i]["mcpPosition"][2])
@@ -43,7 +46,7 @@ for i in range(0 , 5 , 1):
 
     z.append(data[0]["hands"][0]["palmPosition"][2])
 
-    ax.scatter(xs = x , ys = y , zs = z , s = 100)
+    ax.scatter(xs = x , ys = y , zs = z , s = 150)
     ax.plot3D(xs = x , ys = y , zs = z)
 
 plt.show()
